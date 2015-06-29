a9ba3dbfcd52a158c95f2332e1e74fc04b7780d6
Raj <rjkumr@gmail.com>
2015-06-29 16:13:21 -0700

    -

---
+++ javatest/src/general/General.java
@@ -0,0 +1,17 @@
+package general;
+
+import java.math.BigInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class General {
+
+	private final AtomicReference<BigInteger> lastNumber
+			= new AtomicReference<BigInteger>();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
