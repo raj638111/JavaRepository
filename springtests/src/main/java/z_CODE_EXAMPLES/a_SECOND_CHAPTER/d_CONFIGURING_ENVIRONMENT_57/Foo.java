970790529445d602eb820e92cc5d616738189f7a
Raj <rjkumr@gmail.com>
2015-07-02 15:37:57 -0700

    -

---
+++ springtests/src/main/java/z_CODE_EXAMPLES/a_SECOND_CHAPTER/d_CONFIGURING_ENVIRONMENT_57/Foo.java
@@ -0,0 +1,15 @@
+package z_CODE_EXAMPLES.a_SECOND_CHAPTER.d_CONFIGURING_ENVIRONMENT_57;
+
+public class Foo {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
